b62051432db4a2be285a228cb588495654cab8ab
YamaArashi <shadow962@live.com>
2016-09-03 04:11:10 -0700

    eliminate libgcc.s
    (cherry picked from commit 13a00040d707825605d83a6dc33b9162b03f1a10)

--- data/multiboot_berry_glitch_fix.s
+++ data/multiboot_berry_glitch_fix.s
@@ -1,5 +1,8 @@
 	.section .rodata
 
-gMultiBootProgram_BerryGlitchFix_Start::
-	.incbin "data/mb_berry_fix.gba"
+gUnknown_089A6550:: @ 89A6550
+	.incbin "base_emerald.gba", 0x9a6550, 0xc0
+
+gMultiBootProgram_BerryGlitchFix_Start:: @ 89A6610
+	.incbin "base_emerald.gba", 0x9a6610, 0x3b34
 gMultiBootProgram_BerryGlitchFix_End::
